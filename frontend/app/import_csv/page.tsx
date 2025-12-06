7e8fe1595cd91ab8094d5105736fa20c4cc853c8
TAFH-debug <talim2007@mail.ru>
2025-12-07 00:24:52 +0900

    asd

--- frontend/app/import_csv/page.tsx
+++ frontend/app/import_csv/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const router = useRouter();
   
   const onChange = async (e: ChangeEvent) => {
-    await axios.postForm("/csv/uploadfile", {
+    await axios.postForm("/csv/import/", {
       file: e.target.files[0],
     });
 
